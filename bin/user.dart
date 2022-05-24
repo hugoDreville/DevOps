828bc2dee6293930aae2ef23b3c9d28e248c9275
hugoDreville <100281360+hugoDreville@users.noreply.github.com>
2022-05-24 17:20:55 +0200

    Update user.dart

--- bin/user.dart
+++ bin/user.dart
@@ -1,146 +1,185 @@
 import 'dart:io';
 
 class User {
-  static Future<void> createUser(nom) async {
+  static Future<int> createUser(nom) async {
     String cmd = "sudo adduser $nom ";
     Process p = await Process.start("bash", ["-c", cmd]);
-    p.stdin.writeln("btsinfo");
-    print("ok");
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> addGroup(nom, groupe) async {
+  static Future<int> addGroup(nom, groupe) async {
     String cmd = "sudo adduser $nom $groupe";
-    Process p = await Process.start("bash", ["-c", cmd]);
-    await stdout.addStream(p.stdout);
-    await stderr.addStream(p.stderr);
+    ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> afficherUser() async {
+  static Future<int> afficherUser() async {
     String cmd = "  cut -d: -f1 /etc/passwd";
-    Process p = await Process.start("bash", ["-c", cmd]);
-    await stdout.addStream(p.stdout);
-    await stderr.addStream(p.stderr);
+    ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    print(p.stdout);
+    print(p.stderr);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> afficherGroup() async {
+  static Future<int> afficherGroup() async {
     String cmd = "  cut -d: -f1 /etc/group";
-    Process p = await Process.start("bash", ["-c", cmd]);
-    await stdout.addStream(p.stdout);
-    await stderr.addStream(p.stderr);
+    ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    print(p.stdout);
+    print(p.stderr);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> createGroup(nom, gid) async {
+  static Future<int> createGroup(nom, gid) async {
     String cmd = "sudo addgroup $nom -gid $gid";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> delGroup(nom) async {
+  static Future<int> delGroup(nom) async {
     String cmd = "sudo delgroup $nom";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> userdel(nom) async {
+  static Future<int> userdel(nom) async {
     String cmd = "sudo userdel $nom";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> voirInfos(nom) async {
+  static Future<int> voirInfos(nom) async {
     String cmd = "sudo pure-pw show $nom";
-    Process p = await Process.start("bash", ["-c", cmd]);
-    await stdout.addStream(p.stdout);
-    await stderr.addStream(p.stderr);
+    ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    print(p.stdout);
+    print(p.stderr);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> createUserVirtuel(nom, map, uid, gid) async {
+  static Future<int> createUserVirtuel(nom, map, uid, gid) async {
     String cmd =
         "sudo pure-pw useradd $nom -u $uid -g $gid -d /home/$map/$nom -m";
     Process p = await Process.start("bash", ["-c", cmd]);
     await stdout.addStream(p.stdout);
     await stderr.addStream(p.stderr);
     p.stdin.addStream(stdin);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> modifierDownload(nom, i) async {
+  static Future<int> modifierDownload(nom, i) async {
     String cmd = "sudo pure-pw usermod $nom -t $i -m";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> modifierUpload(nom, i) async {
+  static Future<int> modifierUpload(nom, i) async {
     String cmd = "sudo pure-pw usermod $nom -T $i -m";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> modifierFichierMax(nom, i) async {
+  static Future<int> modifierFichierMax(nom, i) async {
     String cmd = "sudo pure-pw usermod $nom -n $i -m";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> modifierTailleMax(nom, i) async {
+  static Future<int> modifierTailleMax(nom, i) async {
     String cmd = "sudo pure-pw usermod $nom -N $i -m";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> modifierAutoriserClient(nom, ip) async {
+  static Future<int> modifierAutoriserClient(nom, ip) async {
     String cmd = "sudo pure-pw usermod $nom -i $ip -m";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> modifierRefuserClient(nom, ip) async {
+  static Future<int> modifierRefuserClient(nom, ip) async {
     String cmd = "sudo pure-pw usermod $nom -I $ip -m";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> modifierAutorisationConnection(nom, ip) async {
+  static Future<int> modifierAutorisationConnection(nom, ip) async {
     String cmd = "sudo pure-pw usermod $nom -r $ip -m";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> modifierRefuserConnection(nom, ip) async {
+  static Future<int> modifierRefuserConnection(nom, ip) async {
     String cmd = "sudo pure-pw usermod $nom -R $ip -m";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> modifierHoraire(nom, i) async {
+  static Future<int> modifierHoraire(nom, i) async {
     String cmd = "sudo pure-pw usermod $nom -z $i -m";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> modifierNombreSessions(nom, i) async {
+  static Future<int> modifierNombreSessions(nom, i) async {
     String cmd = "sudo pure-pw usermod $nom -y $i -m";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> supprimerUtilisateurVirtuel(nom) async {
+  static Future<int> supprimerUtilisateurVirtuel(nom) async {
     String cmd = "sudo pure-pw userdel $nom";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> modifierMotDePasse(nom) async {
+  static Future<int> modifierMotDePasse(nom) async {
     String cmd = "sudo pure-pw passwd $nom";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> lien() async {
+  static Future<int> lien() async {
     String cmd = "ln -s /etc/pure-ftpd/conf/PureDB 60puredb";
     ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> droitRoot(nom) async {
-    String cmd = "sudo chown root:root /home/$nom/";
-    ProcessResult p = await Process.run("bash", ["-c", cmd]);
-  }
-
-  static Future<void> voirUid(nom) async {
+  static Future<int> voirUid(nom) async {
     String cmd = "id -u $nom";
-    Process p = await Process.start("bash", ["-c", cmd]);
-    await stdout.addStream(p.stdout);
-    await stderr.addStream(p.stderr);
+    ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    print(p.stdout);
+    print(p.stderr);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 
-  static Future<void> voirGid(nom) async {
+  static Future<int> voirGid(nom) async {
     String cmd = "id -g $nom";
-    Process p = await Process.start("bash", ["-c", cmd]);
-    await stdout.addStream(p.stdout);
-    await stderr.addStream(p.stderr);
+    ProcessResult p = await Process.run("bash", ["-c", cmd]);
+    print(p.stdout);
+    print(p.stderr);
+    var exitCode = await p.exitCode;
+    return exitCode;
   }
 }
